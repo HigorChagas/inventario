99bfbae0142dd8c0557bfd98a29dbee686959957
HigorChagas <higornchagas@gmail.com>
2023-03-16 12:10:21 -0300

    Implementando função que exibe os dados do banco de dados na página index

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const express = require('express');
+const db = require('./private/js/db');
 const app = express();
-const port = 3000;
+const port = 8080;
+const router = express.Router();
+const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('../views/index');
+
+// Rota da página Index.ejs
+app.get('/', async (req, res) => {
+    const listagem = await db.showInventario();
+
+    res.render('../views/index', {
+        listagem: listagem,
+    });
 });
+
+// app.get('/', (req, res) => {
+//     res.render('../views/home');
+// });
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/private'));
+
+app.get('/add', (req, res) => {
+    res.render('../views/index');
+});
+
+app.post('/add', (req, res) => {
+    res.status(200).send('req.body');
+});
