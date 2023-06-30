d3669f97bcefd3698354a3f0d250e5ace9ef0c85
HigorChagas <higornchagas@gmail.com>
2023-06-30 08:30:32 -0300

    Configurando chaves de segurança e sessões para deploy da aplicação

--- server.js
+++ server.js
@@ -2,7 +2,31 @@ const express = require('express');
 const app = express();
 const routes = require('./src/router/routes');
 const bodyparser = require('body-parser');
-const port = process.env.PORT || 3000;
+const MySQLStore = require('express-mysql-session')(session);
+const session = require('express-session');
+const crypto = require('crypto');
+const PORT = process.env.PORT || 3000;
+
+const generateSessionKey = () => {
+    const key = crypto.randomBytes(32).toString('hex');
+    return key;
+};
+
+const sessionKey = generateSessionKey();
+
+const sessionStore = new MySQLStore({
+    host: process.env.DB_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+router.use(session({
+    secret: sessionKey,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore
+}));
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -16,7 +40,7 @@ app.use((req, res) => {
 });
 // //Localhost
 // app.listen(port, () => {
-//     console.log(`Server running in ${port}`);
+//     console.log(`Server running in ${PORT}`);
 // });
 
 
